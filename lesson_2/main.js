bc7be1aab1459d919f65ba2901164f3f9cf94c7d
Yasmin <yasmindonets17@gmail.com>
2022-07-05 23:07:21 +0200

    not done
    not done

--- lesson_2/main.js
+++ lesson_2/main.js
@@ -1,2 +1,25 @@
-let ololo =`Hello`;
-console.log(`${}`);
+let name = prompt('What is your name?').trim();
+let surname = prompt ('What is your surname?').trim();
+
+
+let email = prompt ('Write your email,please!').replace(/\s/g, '').toLowerCase();
+
+//if(email.startsWith('@')) {
+   //document.write(`not valid email <b>${email}</b> (symbol @ not exist)`);
+//}
+//if(email.startsWith('@')) {
+   //document.write(`not valid email <b>${email}</b> (symbol @ not exist)`);
+//}
+//else if (email.endsWith ('@')){
+//   document.write(`not valid email <b>${email}</b> (symbol @ find in first place)`);
+//}
+
+let dateofBirth = prompt ('What is your date of birth?').replace(/\s/g, '');
+
+document.write(`
+<ul>
+   <li>Full name:  ${name}  ${surname}</li>
+   <li>Email:  ${email}</li>
+   <li>Age:</li>
+</ul>
+`);
