72893f1d243e43e3148ffeab5629c861ddb9feb2
Yasmin <yasmindonets17@gmail.com>
2022-07-05 20:19:31 +0200

    f e
    ebsv

---
+++ Lesson_1/main.js
@@ -0,0 +1,20 @@
+let count = 7; 
+let result =' Значение: '+ 7;
+console.log(result);
+let count2 = count ** 2;
+let result1 = ' Квадрат этого значения: '+  count2;
+console.log(result1);
+let count3 = count ** 3;
+let result2 = ' Куб этого значения: '+ count3
+console.log(result2);
+
+
+let cTemp = 25
+let cToFahr = cTemp * 9 / 5 + 32;cToFahr = cTemp * 9 / 5 + 32;
+let message = cTemp+' \u00B0С равно ' + cToFahr + ' \u00B0F.';
+    console.log(message);
+
+let fTemp = 73;
+let fToCel = (fTemp - 32) * 5 / 9;
+let message2 = fTemp+' \u00B0F равно ' + fToCel + '\u00B0C.';
+console.log(message2);
---
+++ lesson_2/main.js
@@ -0,0 +1,2 @@
+let ololo =`Hello`;
+console.log(`${}`);
