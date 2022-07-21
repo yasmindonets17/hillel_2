f8aace10df1c6f4bb6e3da69c43b8b4c9ca67112
Yasmin <yasmindonets17@gmail.com>
2022-07-21 17:32:41 +0200

    new
    new

--- lesson_5/main.js
+++ lesson_5/main.js
@@ -1,83 +1,98 @@
-// Задание первое Написать программу, которая создает новый массив из произведения(*) значений двух массивов одинакового размер. Также, нужно обработать ситуацию, если массивы будут разной длины.
+//Задача 1 
+// 1 вариант просто с помощью concat
+let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+let result = [].concat(arr[0],arr[1],arr[2] );
+console.log(result);
 
-//Например даны такие массивы:
-//let arr1 = [3, 45, 23, 78, 34];
-//let arr2 = [4, 2, 34, 4,12, 1];
-//то в консоле будет такой вывод:
+// 2 вариант просто с помощью c function
 
-//[12, 90, 782, 312, 408, 1];
+let arr1 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+let flattenArr = arr1.reduce(function(a, b) {
+return a.concat(b);
+});    
+console.log((flattenArr));
 
-let arr1 = [3, 45, 23, 78, 34];
-let arr2 = [4, 2, 34, 4,12, 1];
-let arr3 = [];
-let count;  // вынесла переменную
-  if (arr1.length == arr2.length) {
-    count = arr1.length;
-  } else {
-    count = arr2.length;
-  }
-      for (let i = 0; i < count; i++) {
-        if (arr2[i] === undefined) {
-            arr2.push(1); //изменила  на 1 
-        } else {
-            if (arr1[i] === undefined) {
-              arr1.push(1);
-            }
-        }
-        arr3.push(arr1[i] * arr2[i]);
+//Второе задание 
+
+let inputString = prompt('Введите палидром!');
+function checkPalindrome(inputString) {
+  if(inputString.length == 1){  //Если длина строки равна 1, она становится палиндромом.
+      return true;
+  }else{
+      let i = 0;
+      let j = inputString.length -1;  // Если длина строки нечетная, игнорируйте средний символ
+      while(i < j){
+          if(inputString[i] != inputString[j]){
+              return false;
+          }
+          i++;
+          j--;
       }
-console.log(arr3); // [12, 90, 782, 312, 408, 1]
-
-
-
-//Задание второе: 
-//Написать программу для поиска повторяющихся значений в заданном массиве. 
-//В консоле должен выводиться массив со всеми элементы, которые повторяются более одного раза.
-
-let arrmy = [4,2,34,4,1,12,1,4];
-
-let duplicates = arrmy.reduce(function(acc, el, i, arr) {
-  if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) acc.push(el); return acc;
-}, []);
-
-console.log(duplicates); 
-//вариант второй но тут выводит (3) [1, 4, 4]
-let myArray = [4,2,34,4,1,12,1,4];
-let duplicates1 = []
-let tempArray = [...myArray].sort()
-for (let i = 0; i < tempArray.length; i++){
-   if (tempArray[i + 1] === tempArray [i]) {
-      duplicates1.push(tempArray[i])
-   }
+  }
+  return true;
 }
-console.log(duplicates1);
+console.log(checkPalindrome(`${inputString}`));
 
+//3.Задание
+function sum() {
+  let result = 0;
 
-//Задание 3 
-//Написать программу нахождения количества букв в строке. Пользователь вводит вначале слово, а потом букву. Вывести в консоль количество такой букву в заданной строке.
+  for (let i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+
+  return result;
+}
+console.log( sum(2,4,5)); // 11
+console.log( sum(10,45,34,130)); // 219
+
+///Бонус 4, 5, 6:
+//4.Задание
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+ } 
+console.log(getRandom(1,10)); // random int between 1 to 10
+console.log(getRandom(80,90)); // random int between 80 to 90
+
+//5. Напишите функцию, которая удаляет все вхождения элемента из заданного массива.
 
 //Например:
 
-//let word = 'Banana';
-//let letter = 'n';
-//выводит:
-//The string contains the following number of 'n' letter: 2
+//let arr = [23,56,4,78,5,63,45,210,56];
+//arr = deleteElement(arr, 56)
 
-let word = prompt('Ввести текст', '');
-let letter = prompt('Какую букву ищем?', ''); 
+//console.log(arr); 
 
-function findLetter(str) {
-  let count = 0;
-  for (let char of str.toLowerCase()) {
-    if (letter.includes(char)) {
-      count += 1;
+let numbers = [23,56,4,78,5,63,45,210,56];
+numbers = numbers.filter((n) => {return n != 56});
+console.log(numbers); //[23, 4, 78, 5, 63, 45, 210]
+
+
+function removeItem(arr, num) {
+
+  let store = [];
+
+  for(let i = 0; i < arr.length; i++) {
+
+    if(arr[i] != num){
+    store.push(arr[i]);
     }
   }
-  return count;
+
+  return store;
 }
-document.write (`
-The string contains the following number of ${letter} letter:  
-${findLetter (word)}`);  // поменяла скобки у функции
+console.log(removeItem([23,56,4,78,5,63,45,210,56], 56));   //[23, 4, 78, 5, 63, 45, 210]
+ 
 
 
 
@@ -88,3 +103,97 @@ ${findLetter (word)}`);  // поменяла скобки у функции
 
 
 
+
+
+
+// Массив slice 
+//let number = [1, 2, 3, 4, 5];
+//let other = number.slice(0, 2);
+//console.log(other);
+
+//splice
+/*let number = [1, 2, 3, 4, 5];
+let deleted = number.splice(2, 2);
+console.log(deleted);//удаленное 
+console.log(number);//остаток 
+let number = [1, 2, 3, 4, 5];
+let added = number.splice(0, 0, 10, 20);
+console.log(number);//модифицированное 
+//concat
+let number = [1, 2, 3, 4, 5];
+let number2 = [6, 7, 8, 9, 10];
+let values = [].concat(number, number2);
+console.log(values);*/
+
+//indexOf()
+
+
+/*function showMessage() {
+  alert('Hello World');
+}
+showMessage();   // обязательно вызываем функцию !
+
+let message = "Hello World"; 
+function showMessage() {
+  alert(message);
+}
+showMessage();
+
+let message = "Hello World"; 
+function showMessage(msg) { // переменная создана но в ней ничего не записано, все что написан ов круглых скобках называется параметрами 
+  alert(msg);
+}
+showMessage();
+
+
+
+function showFullName(name, surname) {
+  alert(`${name} ${surname}`);
+}
+showFullName("Alex", "Smith");
+
+let fullName = 'Anna Black'
+function showFullName(name, surname) {
+  let value = surname === undefined ? fullName : surname; 
+  alert(`${name} ${value}`);
+}
+showFullName("Alex");
+
+
+function showMessage(message, yes, no){
+  if(confirm(message)) {
+    yes();
+  } else{
+    no(); 
+  }
+} 
+function sayYes(){
+  alert('You say ok');
+}
+function sayNo(){
+  alert('You say No');
+}
+showMessage('Do you like cats?', sayYes, sayNo);
+
+
+function showFullName(name, surname = 'Black') {
+  alert(`${name} ${surname}`);
+}
+showFullName("Alex", "Smith");
+
+
+function showFullName(message) {
+  alert(message);
+}
+
+function getFullName(name, surname) {
+  return name + '' + surname; 
+}
+
+
+function sayNameToUser(name = 'Alex', surname = 'Smith') {
+  let fullName = getFullName(name, surname);
+  showFullName(fullName);
+}
+sayNameToUser('Alex', 'Smith');
+*/
